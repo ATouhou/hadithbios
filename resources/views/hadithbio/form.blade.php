8ae54b3501bc8a440d758a2090e3c33af0e95595
Denis Gerina <ecarapa@yahoo.com>
2016-04-10 21:50:16 +0000

    Prettier form

--- resources/views/hadithbio/form.blade.php
+++ resources/views/hadithbio/form.blade.php
@@ -1,19 +1,44 @@
-<form action="{{ action('HomeController@postSave') }}" method="post">
-    
-    <div>
-        <label for="name">Name of narrator/transmitter</label>
-        <input type="text" name="name"/>
-    </div>
-    <div>
-        <label for="date_of_birth">Date Of Birth</label>
-        <input type="date" name="date_of_birth" />
-    </div>
-    <div>
-        <label for="date_of_death">Date Of Death</label>
-        <input type="date" name="date_of_death" />
-    </div>
-    <div>
-        <input type="submit" name="submit"/>
-        <input name="_token" type="hidden" value="{{ csrf_token() }}"/>
-    </div>
-</form>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Narrator and Transmitter Bios</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.0/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container">
+    <h2>Narrator and Transmitter Bios</h2>
+    <form class="form-horizontal" role="form" action="{{ action('HomeController@postSave') }}" method="post">
+        <div class="form-group" >
+            <label class="control-label col-sm-2" for="name">Name of narrator / transmitter</label>
+            <div class="col-sm-10">
+                <input class="form-control" type="text" name="name"/>
+            </div>                  
+        </div>
+        <div class="form-group" >
+            <label class="control-label col-sm-2" for="date_of_birth">Date Of Birth</label>
+            <div class="col-sm-10">
+                <input class="form-control" type="date" name="date_of_birth" />
+            </div>                
+        </div>
+        <div class="form-group" >
+            <label class="control-label col-sm-2" for="date_of_death">Date Of Death</label>
+            <div class="col-sm-10">            
+                <input class="form-control" type="date" name="date_of_death" />
+            </div>                  
+        </div>
+        <div class="form-group" >
+            <div class="col-sm-offset-2 col-sm-10">
+                <button type="submit" class="btn btn-default" name="submit"/>Subbmit</button>
+            </div>
+        </div>
+        <input name="_token" type="hidden" value="{{ csrf_token() }}"/>        
+    </form>
+</div>
+
+</body>
+</html>
